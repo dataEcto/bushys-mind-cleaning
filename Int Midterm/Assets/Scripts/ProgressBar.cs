e51fd9287be21ee0046f326e3712cca07a9334dc
Genric Jan Elazegui <gj.elazegui@Gmail.com>
2019-02-24 22:36:19 -0500

    Progress Bar Progress (ha)
    I fixed pretty much all of the logic of the progress bar. Now I should be able to edit it however I like. It has to be done in hard code, but Its fine for now.
    
    I can edit the progress bar to be filled up under certain conditions (mashing to get something done, or mashing before time runs out, ect)

--- Int Midterm/Assets/Scripts/ProgressBar.cs
+++ Int Midterm/Assets/Scripts/ProgressBar.cs
@@ -7,35 +7,37 @@ using UnityEngine.SceneManagement;
 
 //This is code from another game I was attempting
 //Some of the captilzation is off, so I apologize
-public class ProgressBar : MonoBehaviour {
+public class ProgressBar : MonoBehaviour
+{
     /// <summary>
     /// Set Up Variables
     /// </summary>
     public float currentProgress { get; set; }
+
     public float maxProgress { get; set; }
     public float ProgressRefillRate;
 
     //public TextMeshProUGUI progressBarText;
-   public Animator animator;
-   
-   /// <summary>
-   /// In Game used Variables
-   /// </summary>
-   public Slider progressBar;
+    public Animator animator;
 
-   public GameObject player;
-   
-   public GameObject objectOne;
-   private bool oneDone;
-   public GameObject objectTwo;
-   private bool twoDone;
-   public GameObject objectThree;
-   private bool threeDone;
-   
-   
-   public bool shouldFill;
-     
-	void Start ()
+    /// <summary>
+    /// In Game used Variables
+    /// </summary>
+    public Slider progressBar;
+
+    public GameObject player;
+
+    public GameObject objectOne;
+    private bool oneDone;
+    public GameObject objectTwo;
+    private bool twoDone;
+    public GameObject objectThree;
+    private bool threeDone;
+
+
+    public bool shouldFill;
+
+    void Start()
     {
         //can be any value of course
         maxProgress = 100f;
@@ -51,17 +53,17 @@ public class ProgressBar : MonoBehaviour {
         twoDone = false;
         threeDone = false;
     }
-	
 
-	void Update ()
+
+    void Update()
     {
         //Object One
         if (Vector3.Distance(objectOne.transform.position, player.transform.position) < 2f)
         {
             shouldFill = true;
-            animator.SetBool("shouldAppear",true);  
+            animator.SetBool("shouldAppear", true);
         }
-        
+
         if (Input.GetKeyDown(KeyCode.Space) && shouldFill)
         {
             addProgress(10);
@@ -76,13 +78,13 @@ public class ProgressBar : MonoBehaviour {
             oneDone = true;
             shouldFill = false;
             currentProgress = 0;
-            animator.SetBool("shouldAppear",false);
-            objectOne.GetComponent<Animator>().SetBool("isClean",true);
+            animator.SetBool("shouldAppear", false);
+            objectOne.GetComponent<Animator>().SetBool("isClean", true);
         }
         else if (oneDone == true)
         {
-            animator.SetBool("shouldAppear",false);
-         
+            animator.SetBool("shouldAppear", false);
+
         }
 
 
@@ -92,11 +94,11 @@ public class ProgressBar : MonoBehaviour {
             if (Vector3.Distance(objectTwo.transform.position, player.transform.position) < 2f)
             {
                 Debug.Log("Object Two");
-         
+
                 shouldFill = true;
-                animator.SetBool("shouldAppear",true);    
+                animator.SetBool("shouldAppear", true);
             }
-                
+
             if (Input.GetKeyDown(KeyCode.Space) && shouldFill)
             {
                 addProgress(10);
@@ -105,28 +107,63 @@ public class ProgressBar : MonoBehaviour {
             {
                 Debug.Log("Don't Fill");
             }
+
             if (currentProgress >= 99f && twoDone == false)
             {
                 twoDone = true;
                 currentProgress = 0;
-                animator.SetBool("shouldAppear",false);
-                objectTwo.GetComponent<Animator>().SetBool("isCleanTwo",true);
+                animator.SetBool("shouldAppear", false);
+                objectTwo.GetComponent<Animator>().SetBool("isCleanTwo", true);
+            }
+            else if (twoDone == true)
+            {
+                animator.SetBool("shouldAppear", false);
             }
 
-            //Need to set another variable check here?
-           
-            
+            if (twoDone == true)
+            {
+                //Object Three
+                if (Vector3.Distance(objectThree.transform.position, player.transform.position) < 2f)
+                {
+                    Debug.Log("Object Three");
+
+                    shouldFill = true;
+                    animator.SetBool("shouldAppear", true);
+                }
+
+                if (Input.GetKeyDown(KeyCode.Space) && shouldFill)
+                {
+                    addProgress(10);
+                }
+                else if (Input.GetKeyDown(KeyCode.Space) && shouldFill == false)
+                {
+                    Debug.Log("Don't Fill");
+                }
+
+                if (currentProgress >= 99f && twoDone == false)
+                {
+                    threeDone = true;
+                    currentProgress = 0;
+                    animator.SetBool("shouldAppear", false);
+                    objectTwo.GetComponent<Animator>().SetBool("isCleanTwo", true);
+                }
+                else if (threeDone == true)
+                {
+                    animator.SetBool("shouldAppear", false);
+                }
+
+
+            }
+
         }
-        
-        
-               
-	}
+
+    }
 
     float CalculateProgress()
     {
         return currentProgress / maxProgress;
     }
-    
+
     void addProgress(float progressGained)
     {
         Debug.Log("Adding Progress");
@@ -139,10 +176,10 @@ public class ProgressBar : MonoBehaviour {
         {
             currentProgress -= 1;
             Debug.Log("Progress is full. Will no longer add more");
-        }  
-           
+        }
+
     }
-    
+
     void DealDamage(float damageValue)
     {
 
@@ -151,12 +188,11 @@ public class ProgressBar : MonoBehaviour {
         //Same as from start
         progressBar.value = CalculateProgress();
 
-            
+
     }
-
-   
-
-   
-
-    
 }
+
+
+
+
+
